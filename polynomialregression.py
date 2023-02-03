6f8b29ca71c586970b38776981d3e380b5cc4a6f
Manish <adhikari.manish555@gmail.com>
2023-02-03 11:18:43 +0545

    polynomial features added

--- polynomialregression.py
+++ polynomialregression.py
@@ -1,15 +1,23 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.linear_model import LinearRegression
+from sklearn.preprocessing import PolynomialFeatures
 
-X = np.random.rand(100, 1)
-Y = 4 + 5*X + np.random.randn(100, 1)
+X = 4 * np.random.rand(100, 1) - 2
+Y = 4 + 2 * X + 5 * X ** 2 + np.random.randn(100, 1)
+
+poly_features = PolynomialFeatures(degree=2, include_bias=False)
+X_poly = poly_features.fit_transform(X)
 
 reg = LinearRegression()
-reg.fit(X, Y)
+reg.fit(X_poly, Y)
 
-X_values = np.linspace(0, 1, 100).reshape(-1, 1)
-Y_values = reg.predict(X_values)
+X_values = np.linspace(-2, 2, 100).reshape(-1, 1)
+X_values_poly = poly_features.transform(X_values)
+
+Y_values = reg.predict(X_values_poly)
+
+
 plt.scatter(X, Y)
-plt.plot(X_values, Y_values)
+plt.plot(X_values, Y_values, color="red")
 plt.show()
